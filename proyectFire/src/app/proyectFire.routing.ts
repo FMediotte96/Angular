5b173cb90c717becaf33bbe3afb9cab7d0b86f90
Facundo Mediotte <fmediotte@tgv.com.ar>
2018-07-20 14:31:53 -0300

    Rutas

--- proyectFire/src/app/proyectFire.routing.ts
+++ proyectFire/src/app/proyectFire.routing.ts
@@ -5,7 +5,7 @@ import { Peliculas2Component } from './components/peliculas/peliculas2.component
 
 const RUTAS: Routes = [
   { path: 'peliculas', component: PeliculasComponent},
-  { path: 'peliculas2/:id', component: Peliculas2Component},
+  { path: 'peliculas2', component: Peliculas2Component},
   { path: '**', pathMatch: 'full', redirectTo:'peliculas'},
 ];
 
